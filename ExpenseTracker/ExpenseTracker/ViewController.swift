0524d4194e04d7e2627b521fa033e4c2bd306e25
Cody Polton <cj.polton@gmail.com>
2019-11-05 12:41:08 -0600

    Intialized project

---
+++ ExpenseTracker/ExpenseTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExpenseTracker
+//
+//  Created by Cody Polton on 05/11/2019.
+//  Copyright © 2019 Cody Polton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
